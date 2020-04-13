e6e292646b224c7c1fd31ee8ae018a73255cf069
Kevin <kevinrisqi22@gmail.com>
2020-04-13 22:45:25 +0700

    Update in Course Functional

---
+++ Control Flow/Enumeration/Enumeration/src/App.kt
@@ -0,0 +1,9 @@
+// main function
+fun main() {
+    val color : Color = Color.Kawasaki
+    print(color)
+}
+
+enum class Color{
+    RED, GREEN, BLUE, Kawasaki
+}
---
+++ Kotlin Fundamental/Numbers/Number operation/src/App.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val numberOne = 1
+    val numberTwo = 2
+
+    println(numberOne + numberTwo)
+
+    println(numberOne / numberTwo)
+
+    println(5 + 4 * 4)
+
+    println((5 + 4) * 4)
+
+}
+
+//3
+//0,5
+//36
+//36
+
+
